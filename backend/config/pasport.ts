5bc40ad827a7e59c9d93daeb68285933fe9a2f5b
Akshat Vyas <akshatmanojvyas@gmail.com>
2025-04-18 17:15:48 -1200

    removing all google clint info

--- backend/config/pasport.ts
+++ backend/config/pasport.ts
@@ -2,18 +2,27 @@
 
 
 
-import { google } from 'googleapis';
+
 
 /**
  * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
  * from the client_secret.json file. To get these credentials for your application, visit
  * https://console.cloud.google.com/apis/credentials.
  */
-const GOOGLE_CLIENT_ID = '188177420369-4euf5cq0dhcc8pl18t8ghe284rka9b6f.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+// const GOOGLE_CLIENT_ID = '188177420369-4euf5cq0dhcc8pl18t8ghe284rka9b6f.apps.googleusercontent.com';
+// const GOOGLE_CLIENT_SECRET = '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';
+import { google } from 'googleapis';
+import dotenv from 'dotenv'
+// Load environment variables from the .env file
+dotenv.config();
 
- export const oauth2Client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    'postmessage'
+// Retrieve credentials from environment variables
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+export const oauth2Client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  REDIRECT_URI
 );
